fe5c203901a0b6fe444fe9d10c6399cf75935274
luuguas <69027878+luuguas@users.noreply.github.com>
2023-06-11 19:53:11 +0900

    Update code.js - convert to jQuery / add color to checkbox

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           AtCoder Listing Tasks
 // @namespace      https://github.com/luuguas/AtCoderListingTasks
-// @version        1.2.1
+// @version        1.3
 // @description    [問題]タブをクリックすると、各問題のページに移動できるドロップダウンリストを表示します。
 // @description:en Click [Tasks] tab to open a drop-down list linked to each task.
 // @author         luuguas
@@ -11,6 +11,9 @@
 // @exclude        https://atcoder.jp/contests/archive
 // @grant          none
 // ==/UserScript==
+
+//AtCoderに標準で読み込まれているjQueryを使用
+let $ = window.jQuery;
 
 (function() {
     'use strict';
@@ -22,7 +25,12 @@
         'label': `width: 100%; margin: 0px; padding: 3px 10px; clear: both; font-weight: normal; white-space: nowrap;`,
         'checkbox': `margin: 0px; vertical-align: middle;`,
     };
-
+    const TEXTS = {
+        'new_tab': {'ja': ' 新しいタブで開く', 'en': ' Open in a new tab'},
+        'all_tasks': {'ja': ' 問題一覧', 'en': ' All Tasks'},
+        'loading_failed': {'ja': '(読み込み失敗)', 'en': '(Loading Failed)'},
+    };
+    
     /* 設定の読み込み・保存をするクラス */
     const SETTING_KEY = 'Setting_AtCoderListingTasks';
     let Setting = function(){
@@ -52,54 +60,48 @@
         },
     };
     let setting = new Setting();
-
-    /* ページの言語を取得 */
-    let content_language = document.querySelector('meta[http-equiv="Content-Language"]');
-    let lang = 'ja';
-    if(content_language !== null && content_language.getAttribute('content') === 'en'){
-        lang = 'en';
+    
+    /* ページの言語を取得する関数 */
+    function getLanguage(){
+        lang = 'ja';
+        let content_language = $('meta[http-equiv="Content-Language"]');
+        if(content_language.length !== 0 && content_language.attr('content') === 'en'){
+            lang = 'en';
+        }
     }
-
+    let lang = 'ja';
+    
     /* [問題]タブに本UserScript用のidを追加する関数(タブがなければfalseを返す) */
     function attachId(){
-        let tabs = document.getElementById('contest-nav-tabs');
-        if(tabs === null){
+        let tabs = $('#contest-nav-tabs');
+        if(tabs.length === 0){
             return false;
         }
-        let tasks_tab = tabs.querySelector('a[href$="tasks"]');
-        if(tasks_tab === null){
+        let tasks_tab = tabs.find('a[href$="tasks"]');
+        if(tasks_tab.length === 0){
             return false;
         }
         else{
-            tasks_tab.id = `${TAG_PREFIX}-tab`;
+            tasks_tab.attr('id', `${TAG_PREFIX}-tab`);
             return true;
         }
     }
-
+    
     /* [問題]タブを[提出結果]タブと同様のドロップダウンリストに変える関数 */
     function togglize(){
-        let tasks_tab = document.getElementById(`${TAG_PREFIX}-tab`);
-        let attr = {
+        let tasks_tab = $(`#${TAG_PREFIX}-tab`);
+        tasks_tab.attr({
             'class': 'dropdown-toggle',
             'data-toggle': 'dropdown',
             'href': '#',
             'role': 'button',
             'aria-haspopup': 'true',
             'aria-expanded': 'false',
-        };
-        for(let [key, value] of Object.entries(attr)){
-            tasks_tab.setAttribute(key, value);
-        }
-        let caret = document.createElement('span');
-        caret.className = 'caret';
-        tasks_tab.appendChild(caret);
-
-        let dropdown_menu = document.createElement('ul');
-        dropdown_menu.className = `dropdown-menu`;
-        dropdown_menu.style = STYLES['dropdown'];
-        tasks_tab.parentNode.appendChild(dropdown_menu);
+        });
+        tasks_tab.append($('<span>', {class: 'caret'}));
+        tasks_tab.parent().append($('<ul>', {class: 'dropdown-menu', style: STYLES['dropdown']}));
     }
-
+    
     /* [[✓]新しいタブで開く]のチェックの有無でaタグの属性を切り替える関数 */
     function changeNewTabAttr(e){
         let a = e.currentTarget;
@@ -112,129 +114,89 @@
             a.rel = '';
         }
     }
-
+    
     /* リストを追加する関数 */
     function addList(){
-        let url = location.href;
-        let contest_name = url.split('/')[4];
+        let contest_name = location.href.split('/')[4];
         //contest_nameにハッシュ(#?)があれば取り除く
         let hash = contest_name.search(/[#\?]/);
         if(hash !== -1){
             contest_name = contest_name.slice(0, hash);
         }
+        
+        let dropdown_menu = $(`#${TAG_PREFIX}-tab`).parent().children('.dropdown-menu');
 
-        let tasks_tab_li = document.getElementById(`${TAG_PREFIX}-tab`).parentNode;
-        let dropdown_menu = tasks_tab_li.querySelector('.dropdown-menu');
-
+        /* [問題一覧]の追加 */
+        let all_tasks = $('<a>', {href: `${CONTEST_URL}${contest_name}/tasks`});
+        all_tasks.append($('<span>', {class: 'glyphicon glyphicon-list'}).attr('aria-hidden', 'true'));
+        all_tasks.append(document.createTextNode(TEXTS['all_tasks'][lang]));
+        //チェックボックスにチェックが付いていたら新しいタブで開く
+        all_tasks.on('click', changeNewTabAttr);
+        dropdown_menu.append($('<li>').append(all_tasks));
+        
         /* [[✓]新しいタブで開く]の追加 */
-        let new_tab = document.createElement('li');
-        let label = document.createElement('label');
-        label.style = STYLES['label'];
-
-        let checkbox = document.createElement('input');
-        checkbox.style = STYLES['checkbox'];
-        checkbox.setAttribute('type', 'checkbox');
+        let label = $('<label>', {style: STYLES['label']});
+        label.css('color', all_tasks.css('color')); //[問題一覧]から色情報を取得
+        let checkbox = $('<input>', {type: 'checkbox', style: STYLES['checkbox']});
         //チェックボックスはチェック状態をストレージと同期
         setting.loadSetting();
-        checkbox.checked = setting.getValue('new_tab');
-        checkbox.addEventListener('click', function(){
+        checkbox.prop('checked', setting.getValue('new_tab'));
+        checkbox.on('click', function(){
             setting.setValue('new_tab', this.checked);
         });
-        label.appendChild(checkbox);
-
-        if(lang === 'ja'){
-            label.appendChild(document.createTextNode(' 新しいタブで開く'));
-        }
-        else if(lang === 'en'){
-            label.appendChild(document.createTextNode(' Open in a new tab'));
-        }
-        new_tab.appendChild(label);
-        dropdown_menu.appendChild(new_tab);
+        label.append(checkbox);
+        label.append(document.createTextNode(TEXTS['new_tab'][lang]));
+        dropdown_menu.prepend($('<li>').append(label));
         //チェックボックスが押された場合はドロップダウンリストを非表示にしない
-        dropdown_menu.addEventListener('click', function(e){
-            if(e.target === label){
+        dropdown_menu.on('click', function(e){
+            if(e.target === label[0]){
                 e.stopPropagation();
             }
         });
-
-        /* [問題一覧]の追加 */
-        let all_tasks = document.createElement('li');
-        let a = document.createElement('a');
-        a.href = `${CONTEST_URL}${contest_name}/tasks`;
-        if(lang === 'ja'){
-            a.innerHTML = `<span class="glyphicon glyphicon-list" aria-hidden="true"></span> 問題一覧`;
-        }
-        else if(lang === 'en'){
-            a.innerHTML = `<span class="glyphicon glyphicon-list" aria-hidden="true"></span> All Tasks`;
-        }
-
-        //チェックボックスにチェックが付いていたら新しいタブで開く
-        a.addEventListener('click', changeNewTabAttr);
-        all_tasks.appendChild(a);
-        dropdown_menu.appendChild(all_tasks);
-
+        
         /* 分割線の追加 */
-        let divider = document.createElement('li');
-        divider.className = 'divider';
-        dropdown_menu.appendChild(divider);
-
+        dropdown_menu.append($('<li>', {class: 'divider'}));
+        
         /* 各問題の追加 */
         // https://atcoder.jp/contests/.../tasks のページ情報をリクエスト
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'document';
         xhr.onreadystatechange = function(){
-            let tasks_tab_li = document.getElementById(`${TAG_PREFIX}-tab`).parentNode;
-            let dropdown_menu = tasks_tab_li.querySelector('.dropdown-menu');
-
+            let dropdown_menu = $(`#${TAG_PREFIX}-tab`).parent().children('.dropdown-menu');
+            
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
-                    let result = xhr.responseXML;
-                    let problem_node = result.querySelector('#contest-nav-tabs + .col-sm-12');
-                    let problem_list = problem_node.querySelectorAll('table.table tbody tr');
-
+                    let result = $(xhr.responseXML);
+                    let problem_node = result.find('#contest-nav-tabs + .col-sm-12');
+                    let problem_list = problem_node.find('tbody tr');
+                    
                     //表から問題情報を抽出
                     let list = [];
-                    for(let li of problem_list){
-                        let td = li.querySelectorAll('td');
-                        let problem_url = td[0].firstChild.getAttribute('href');
-                        let problem_diff = td[0].firstChild.textContent;
-                        let problem_name = td[1].firstChild.textContent;
+                    problem_list.each(function(){
+                        let td = $(this).children('td');
                         list.push({
-                            url: problem_url,
-                            diff: problem_diff,
-                            name: problem_name,
+                            url: td[0].firstChild.getAttribute('href'),
+                            diff: td[0].firstChild.textContent,
+                            name: td[1].firstChild.textContent,
                         });
-                    }
-
+                    });
+                    
                     //リストを追加
                     for(let data of list){
-                        let li = document.createElement('li');
-                        let a = document.createElement('a');
-                        a.setAttribute('href', data.url);
-                        a.textContent = `${data.diff} - ${data.name}`;
+                        let a = $('<a>', {href: data.url, text: `${data.diff} - ${data.name}`});
                         //チェックボックスにチェックが付いていたら新しいタブで開く
-                        a.addEventListener('click', changeNewTabAttr);
-                        li.appendChild(a);
-                        dropdown_menu.appendChild(li);
+                        a.on('click', changeNewTabAttr);
+                        dropdown_menu.append($('<li>').append(a));
                     }
                     console.log('[AtCoder Listing Tasks] Succeeded!');
                 }
                 else{
                     //エラー情報を追加
-                    let li = document.createElement('li');
-                    let a = document.createElement('a');
-                    a.setAttribute('href', '#');
-                    if(lang === 'ja'){
-                        a.textContent = '(読み込み失敗)';
-                    }
-                    else if(lang === 'en'){
-                        a.textContent = '(Loading Failed)';
-                    }
-                    a.addEventListener('click', function(e){
+                    let a = $('<a>', {href: '#', text: TEXTS['loading_failed'][lang]});
+                    a.on('click', function(e){
                         e.preventDefault();
                     });
-                    li.appendChild(a);
-                    dropdown_menu.appendChild(li);
+                    dropdown_menu.append($('<li>').append(a));
                     console.log('[AtCoder Listing Tasks] Failed...');
                 }
             }
@@ -242,7 +204,7 @@
         xhr.open('GET', `${CONTEST_URL}${contest_name}/tasks`, true);
         xhr.send(null);
     }
-
+    
     /* スクリプトを実行 */
     let tab_exist = attachId();
     //タブがない場合は終了
@@ -250,7 +212,8 @@
         console.log('[AtCoder Listing Tasks] [Tasks] Tab isn\'t exist.');
         return;
     }
+    getLanguage();
     togglize();
     addList();
-
+    
 })();
